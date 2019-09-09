32103cbc5f5525d22f4b084630def91cabf3d530
Mark Wragg <mark.wragg@elysianit.com>
2019-09-09 21:29:18 +0100

    Moved class to individual file

--- MacNotify/Public/Invoke-AlerterNotification.ps1
+++ MacNotify/Public/Invoke-AlerterNotification.ps1
@@ -1,18 +1,3 @@
-if (-not ('SoundNames' -as [type])) {
-    
-    Class SoundNames : System.Management.Automation.IValidateSetValuesGenerator {
-        [String[]] GetValidValues() {
-
-            $SoundNames = ForEach ($SoundPath in '/System/Library/Sounds/', '/Library/Sounds', '~/Library/Sounds') {
-                if (Test-Path $SoundPath) {
-                    (Get-ChildItem $SoundPath).BaseName
-                }
-            }
-            return [string[]] $SoundNames
-        }
-    }
-}
-
 Function Invoke-AlerterNotification {
     <#
         .SYNOPSIS
